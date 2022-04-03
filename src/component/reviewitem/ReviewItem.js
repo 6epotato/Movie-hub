066d72b1ae0429912b2d5e24b73fd0f3b3e60a25
6epotato <aarifurrahman93@gmail.com>
2022-04-04 02:13:26 +0600

    review section done

--- src/component/reviewitem/ReviewItem.js
+++ src/component/reviewitem/ReviewItem.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import './ReviewItem.css';
 
 const ReviewItem = ({ review }) => {
-    const { name } = review;
+    const { name, rating, description, img } = review;
     return (
-        <div>
-            <h1>{name}</h1>
+        <div className='pl-7 review-item'>
+            <div className='px-9 py-5'>
+                <img src={img} alt="" />
+                <h1 className='font-medium text-2xl'>{name}</h1>
+                <p><span className='text-xs'>Rating Out of 5 :</span> <span className='font-bold'>{rating}</span></p>
+                <p>{description}</p></div>
         </div>
     );
 };
